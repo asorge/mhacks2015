8f997a11423fd8e5efb583d0fc840b726f8bd9f3
Maggie Yu <shuangy@andrew.cmu.edu>
2015-09-07 16:15:02 -0400

    added validations to models

--- app/models/job_listing.rb
+++ app/models/job_listing.rb
@@ -1,4 +1,8 @@
 class JobListing < ActiveRecord::Base
 	# Relationships
 	belongs_to :company
+
+	# Validations
+	validates_presence_of :title, :description, :location, :url, :date_posted, :contact
+	validates_date :date_posted
 end
--- app/models/user_interest.rb
+++ app/models/user_interest.rb
@@ -2,4 +2,8 @@ class UserInterest < ActiveRecord::Base
 	# Relationships
 	belongs_to :user 
 	belongs_to :company
+
+	# Validations
+	validates_presence_of :rating
+	validates_numericality_of :ratinig, greater_than: 0
 end
