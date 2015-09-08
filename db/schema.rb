bc2963b40dfb70f176cb7f542eec2d6b8ec7a82c
Maggie Yu <shuangy@andrew.cmu.edu>
2015-09-07 20:30:38 -0400

    changed data type of rating to integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912100936) do
+ActiveRecord::Schema.define(version: 20150912100937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20150912100936) do
   create_table "user_interests", force: :cascade do |t|
     t.integer  "company_id"
     t.integer  "user_id"
-    t.decimal  "rating"
+    t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
