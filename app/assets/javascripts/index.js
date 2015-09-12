0f98f731c7f447313445cc4df74ed918a169f8d3
Ally Sorge <asorge@andrew.cmu.edu>
2015-09-12 14:25:52 -0400

    log in form

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,32 +1,31 @@
 // MAIN JS FILE
 
 $( document ).ready(function() {
-	if (IN.User.isAuthorized()) {
-		logInSuccess();
-	}
-	else {
-		init();
-	}
+	init();
 });
 
 function init() {
-	$('#linkedinButton').click(function() {
-		console.log('linkedinButton click');
-		IN.User.authorize(logInSuccess());
-	});
-	$('#linkedinLogOut').click(function() {
-		IN.User.logout();
-	});
+	if (IN.User.isAuthorized()) {
+		$('#linkedinButton').click(function() {
+			IN.User.authorize(logInSuccess());
+		});
+	}
+	else {
+		$('#linkedinLogOut').click(function() {
+			IN.User.logout();
+		});
+	}
 }
 
 function logInSuccess() {
-	alert('logged in!');
 	IN.Event.on(IN, "auth", getProfileData);
 }
 
     // Handle the successful return from the API call
 function onSuccess(data) {
     console.log(data);
+    var data = data;
+    fillInForm();
 }
 
 // Handle an error response from the API call
@@ -37,4 +36,13 @@ function onError(error) {
 // Use the API call wrapper to request the member's basic profile data
 function getProfileData() {
     IN.API.Raw("/people/~").result(onSuccess).error(onError);
+    $.getJSON(IN.API.Raw("/people/~"), function(data) {
+    	$.each(data, function(key, value) {
+    		var field = $("#" + key);
+    		if (field.length) {
+    			field.val(value);
+    		}
+    	})
+    })
 }
+
