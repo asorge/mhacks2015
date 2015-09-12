bf81cd214a0f6eaff2973bedf739d951b6a99959
Ally Sorge <asorge@andrew.cmu.edu>
2015-09-12 13:50:27 -0400

    Adding linked in log out ability

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,12 @@
 // MAIN JS FILE
 
 $( document ).ready(function() {
-	init();
+	if (IN.User.isAuthorized()) {
+		logInSuccess();
+	}
+	else {
+		init();
+	}
 });
 
 function init() {
@@ -9,24 +14,27 @@ function init() {
 		console.log('linkedinButton click');
 		IN.User.authorize(logInSuccess());
 	});
+	$('#linkedinLogOut').click(function() {
+		IN.User.logout();
+	});
+}
 
-	function logInSuccess() {
-		alert('logged in!');
-		IN.Event.on(IN, "auth", getProfileData);
-	}
+function logInSuccess() {
+	alert('logged in!');
+	IN.Event.on(IN, "auth", getProfileData);
+}
 
-	    // Handle the successful return from the API call
-    function onSuccess(data) {
-        console.log(data);
-    }
+    // Handle the successful return from the API call
+function onSuccess(data) {
+    console.log(data);
+}
 
-    // Handle an error response from the API call
-    function onError(error) {
-        console.log(error);
-    }
+// Handle an error response from the API call
+function onError(error) {
+    console.log(error);
+}
 
-    // Use the API call wrapper to request the member's basic profile data
-    function getProfileData() {
-        IN.API.Raw("/people/~").result(onSuccess).error(onError);
-    }
+// Use the API call wrapper to request the member's basic profile data
+function getProfileData() {
+    IN.API.Raw("/people/~").result(onSuccess).error(onError);
 }
