fbf690b260288840272fb778fd34822a25b890a9
Ally Sorge <asorge@andrew.cmu.edu>
2015-09-12 19:51:51 -0400

    deleted repeat code blocl

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,15 +3,15 @@
 $( document ).ready(function() {
 	init();
 
-	  var indeed_client = new Indeed("3193862925521655");
-      indeed_client.search({
-        q: 'javascript',
-        l: 'austin',
-        userip: '1.2.3.4',
-        useragent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)',
-      }, function(search_response){
-          console.log(search_response);
-      });
+	  // var indeed_client = new Indeed("3193862925521655");
+   //    indeed_client.search({
+   //      q: 'javascript',
+   //      l: 'austin',
+   //      userip: '1.2.3.4',
+   //      useragent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)',
+   //    }, function(search_response){
+   //        console.log(search_response);
+   //    });
 });
 
 function init() {
