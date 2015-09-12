873b96d30338cd943c796ec3279250adf4700908
Maggie Yu <shuangy@andrew.cmu.edu>
2015-09-12 03:21:00 -0400

    created home controller

--- jobhacks/config/routes.rb
+++ jobhacks/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/home'
+
   resources :applications
   resources :job_listings
   resources :companies
@@ -8,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'applications#index'
+  root 'applications#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
