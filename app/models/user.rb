d3c9c9b6d6bbf155aa8a9309b1902accdffe26f9
Maggie Yu <shuangy@andrew.cmu.edu>
2015-09-12 07:04:38 -0400

    logos done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
 
 	# Validations
   	validates_presence_of :last_name, :first_name, :email
-  	validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only"
   	validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format"
   	validates :username, presence: true, uniqueness: { case_sensitive: false}
 	validates_presence_of :password, on: :create 
